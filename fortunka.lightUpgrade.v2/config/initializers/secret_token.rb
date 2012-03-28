da573e2433b5e96e09f0fb700ed9fdd479b6cb7a
Miłosz Osiński <mylosz@yahoo.pl>
2012-03-29 01:17:35 +0200

    małe poprawki i dodatkowe opcje

---
+++ fortunka.lightUpgrade.v2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FortunkaLightupgrade::Application.config.secret_token = '99d732ce08dd8a095ab6da7d6ec63fef4737c7f00db7ffdfbb7b87c5eb52381619716779444cd9a03fb00d8d22c89b2863bb0cca95767b4e953741a861247c68'
